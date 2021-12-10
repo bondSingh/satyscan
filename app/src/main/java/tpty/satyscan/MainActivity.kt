cfbdddaeea295a2e60721aba82ecf8e332273569
bondSingh <1202satish@gmail.com>
2021-12-11 02:40:36 +0530

    Adding Scanner related code in Seperate module.

--- app/src/main/java/tpty/satyscan/MainActivity.kt
+++ app/src/main/java/tpty/satyscan/MainActivity.kt
@@ -2,10 +2,30 @@ package tpty.satyscan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import tpty.satyscan.databinding.ActivityMainBinding
+import android.content.Intent
+import saty.scanner.CameraActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textScan.setOnClickListener {
+            val myIntent = Intent(this@MainActivity, CameraActivity::class.java)
+            startActivity(myIntent)
+            Log.d("saty", "MainActivity onCreate:: textScan")
+
+        }
+        binding.imageScan.setOnClickListener {
+            Log.d("saty", "MainActivity onCreate:: imageScan")
+
+        }
     }
 }
